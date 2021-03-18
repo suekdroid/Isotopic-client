cab2cb26a52b082e252ae3e1cb582dc8de193ed2
suekdroid <nikolajskousen@gmail.com>
2021-03-18 23:14:55 +0100

    CLIENT opacity fix

--- src/api/HttpClient.ts
+++ src/api/HttpClient.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000/api'
+// const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/'
+
+const BASE_URL = 'https://isotopic.herokuapp.com/api'
 
 const client = axios.create({
     baseURL: BASE_URL,
